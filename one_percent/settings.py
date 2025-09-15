facc027a8cc4a218d4c2a388786f2266e480f809
Alex <alexthomas1912@gmail.com>
2025-09-15 03:45:49 +0100

    Create custom SECRET_KEY to secure databases

--- one_percent/settings.py
+++ one_percent/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xq!+*3xk7w+idtu)q9_=t9n^i$*e7mueg(b1$k%g9bpv+*@s#2'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
