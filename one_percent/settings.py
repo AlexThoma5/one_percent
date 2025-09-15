10a27d2f8635e8127fb4c20d976f5304a464158b
Alex <alexthomas1912@gmail.com>
2025-09-15 03:03:41 +0100

    Ready code for deployment to heroku

--- one_percent/settings.py
+++ one_percent/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-xq!+*3xk7w+idtu)q9_=t9n^i$*e7mueg(b1$k%g9bpv+*@s#2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com','127.0.0.1']
 
 
 # Application definition
