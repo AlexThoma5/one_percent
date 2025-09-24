c13dab6f1ddab71aed6564f5f388d2703a69c37b
Alex <alexthomas1912@gmail.com>
2025-09-24 01:53:45 +0100

    Add category model to categories app, run migrations and register model in admin panel for manual category management

--- categories/models.py
+++ categories/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Category(models.Model):
+    """
+    Stores a single category
+    """
+    name = models.CharField(max_length=200, unique=True)
+    icon = models.CharField(max_length=200, default='fa-regular fa-pen-to-square')
+    slug = models.SlugField(max_length=200, unique=True)
+
+    def __str__(self):
+        return self.name
