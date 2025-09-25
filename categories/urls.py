5c96abc4bad208fe42c4b4a8ea024d5282490e7c
Alex <alexthomas1912@gmail.com>
2025-09-25 18:39:22 +0100

    Add category detail view with slug-based URL and link from dashboard to corresponding category_detail page

--- categories/urls.py
+++ categories/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('<slug:slug>/', views.category_detail, name='category_detail'),
     path('test/', views.test_view, name='categories-test'),
 ]
--- categories/views.py
+++ categories/views.py
@@ -1,8 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Category
 
 # Create your views here.
 
 
 def test_view(request):
-    return HttpResponse("This is a test view for categories app") # Temporary response for testing
+    return HttpResponse("This is a test view for categories app")  # Temporary response for testing
+
+
+def category_detail(request, slug):
+    """
+    Display an individual :model:`categories.Category`.
+
+    **Context**
+
+    ``category``
+        An instance of :model:`categories.Category`.
+    **Template:**
+
+    :template:`categories/category_detail.html`
+    """
+
+    queryset = Category.objects.all()
+    category = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request,
+        "categories/category_detail.html",
+        {"category": category},
+    )
