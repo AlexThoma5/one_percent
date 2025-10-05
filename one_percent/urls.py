42d7e652d7bd61cb7c5a056d2db9f5ab76d843de
Alex <alexthomas1912@gmail.com>
2025-10-05 17:41:08 +0100

    Add Django AllAuth for user authentication and integrate with project settings, configure URLs and templates

--- one_percent/urls.py
+++ one_percent/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path("accounts/", include("allauth.urls")),
     path('admin/', admin.site.urls),
     path('categories/', include('categories.urls'), name='categories-urls'),
     path('', include('dashboard.urls'), name='dashboard-urls')  # Root level page is going to be dashboard
