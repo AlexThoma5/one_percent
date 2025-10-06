43a7a9eadf63a31f54ffcaea11f17e47f46ce248
Alex <alexthomas1912@gmail.com>
2025-10-06 02:03:26 +0100

    Add user foreign key to Log model to allow user specific logs

--- categories/models.py
+++ categories/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -17,10 +18,11 @@ class Category(models.Model):
 
 class Log(models.Model):
     """
-    Stores a single log entry
+    Stores a single log entry related to :model:`auth.user`.
     """
     title = models.CharField(max_length=50)
     content = models.TextField(blank=True)  # Optional field for extra notes
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="logs")
@@ -29,4 +31,4 @@ class Log(models.Model):
         ordering = ["-created_on"]
 
     def __str__(self):
-        return f'{self.title} ({self.category})'
+        return f'{self.title} ({self.category}) made by {self.user}'
