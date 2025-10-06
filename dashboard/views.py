e7b78bf931d1af03209886d76d5c50f594e7b77a
Alex <alexthomas1912@gmail.com>
2025-10-06 01:46:23 +0100

    Add login_required to dashboard views to restrict dashboard to authenticated users only

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 from categories.models import Category
 
 # Create your views here.
 
-
+@login_required
 def dashboard_view(request):
     categories = Category.objects.all()
     return render(request, "dashboard/dashboard.html", {"categories": categories})
