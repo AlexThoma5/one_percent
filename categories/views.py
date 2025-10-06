fdd66847b21e1ac64a1a4a300b13ce1054e20997
Alex <alexthomas1912@gmail.com>
2025-10-06 02:18:58 +0100

    Add to category_detail view to retrict logs to current user and assign user on log creation

--- categories/views.py
+++ categories/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 from .models import Category, Log
 from .forms import LogForm
 
@@ -9,7 +9,7 @@ from .forms import LogForm
 def test_view(request):
     return HttpResponse("This is a test view for categories app")  # Temporary response for testing
 
-
+@login_required
 def category_detail(request, slug):
     """
     Display an individual :model:`categories.Category`.
@@ -19,7 +19,9 @@ def category_detail(request, slug):
     ``category``
         An instance of :model:`categories.Category`.
     ``logs``
-        All logs in relation to the category`.
+        All logs in relation to the category tied to individual user`.
+    ``log_form``
+    An instance of :form:`categories.LogForm`
     **Template:**
 
     :template:`categories/category_detail.html`
@@ -27,12 +29,13 @@ def category_detail(request, slug):
 
     queryset = Category.objects.all()
     category = get_object_or_404(queryset, slug=slug)
-    logs = category.logs.all().order_by('-created_on')
+    logs = category.logs.filter(user=request.user).order_by('-created_on')
 
     if request.method == "POST":
         log_form = LogForm(data=request.POST)
         if log_form.is_valid():
             log = log_form.save(commit=False)
+            log.user = request.user
             log.category = category
             log.save()
 
