1305b0d7e83ec210f8b7ba5f975563660bf77c67
Alex <alexthomas1912@gmail.com>
2025-10-12 17:30:49 +0100

    Add order field and Meta class to Category model to allow site admin to control the order of the categories

--- categories/models.py
+++ categories/models.py
@@ -11,6 +11,10 @@ class Category(models.Model):
     name = models.CharField(max_length=200, unique=True)
     icon = models.CharField(max_length=200, default='fa-regular fa-pen-to-square')
     slug = models.SlugField(max_length=200, unique=True)
+    order = models.PositiveIntegerField(default=1)
+
+    class Meta:
+        ordering = ["order"]
 
     def __str__(self):
         return self.name
