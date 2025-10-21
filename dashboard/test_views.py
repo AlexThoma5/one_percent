9d1994b67712692c55f4d0ff8932dc9cd6babd33
Alex <alexthomas1912@gmail.com>
2025-10-21 19:35:34 +0100

    Add automated view tests for dashboard, and landing pages with context, and templates

--- dashboard/test_views.py
+++ dashboard/test_views.py
@@ -1,7 +1,6 @@
 from django.contrib.auth.models import User
 from django.urls import reverse
 from django.test import TestCase
-from categories.models import Category
 
 
 class TestHomeView(TestCase):
@@ -22,3 +21,37 @@ class TestHomeView(TestCase):
     def test_redirects_anon_user_to_landing(self):
         response = self.client.get(reverse('home'))
         self.assertRedirects(response, reverse('landing'))
+
+
+class TestDashboardView(TestCase):
+
+    def setUp(self):
+        """ Creates user instance for tests"""
+        self.user = User.objects.create_user(
+            username="myUsername",
+            password="myPassword",
+            email="test@test.com"
+        )
+
+    def test_render_dashboard_page_with_categories(self):
+        """ Test for dashboard page rendering with correct context
+        and template """
+        self.client.login(username="myUsername", password="myPassword")
+        response = self.client.get(reverse('dashboard'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "dashboard/dashboard.html")
+
+        # checks for content and context
+        self.assertIn(b"myUsername", response.content)
+        self.assertIn("categories", response.context)
+        self.assertIn("log_count", response.context)
+        self.assertIn("labels", response.context)
+        self.assertIn("data", response.context)
+
+
+class TestLandingView(TestCase):
+    """ Test for landing page rendering with correct template """
+    def test_renders_landing_template(self):
+        response = self.client.get(reverse('landing'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "dashboard/landing.html")
