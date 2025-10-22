c6f75918c8eecd1e6880d412bfd3ce7cfa53078c
Alex <alexthomas1912@gmail.com>
2025-10-22 23:29:44 +0100

    Add docstrings to dashboard views

--- dashboard/views.py
+++ dashboard/views.py
@@ -6,6 +6,10 @@ from categories.models import Category
 
 
 def home_view(request):
+    """
+    'home' redirects user to 'dashboard' if authenticated or 'landing'
+    if they are not authenticated.
+    """
     if request.user.is_authenticated:
         return redirect('dashboard')
     else:
@@ -14,6 +18,25 @@ def home_view(request):
 
 @login_required
 def dashboard_view(request):
+    """
+    Display the user's dashboard with all :model:`categories.Category` objects.
+
+    **Context**
+
+    ``categories``
+        All instances of :model:`categories.Category`.
+    ``log_count``
+        A zipped iterable of each category name and the number of logs
+        created by the current user.
+    ``labels``
+        A list of category names used for Chart.js data labels.
+    ``data``
+        A list of log counts corresponding to each category.
+
+    **Template:**
+
+    :template:`dashboard/dashboard.html`
+    """
     categories = Category.objects.all()
 
     # Chatgpt helped with chart.js setup
@@ -36,4 +59,7 @@ def dashboard_view(request):
 
 
 def landing_view(request):
+    """
+    Renders landing.html template
+    """
     return render(request, "dashboard/landing.html")
