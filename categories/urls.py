a100675ffd6dc94eaf855066397295fd220a39d1
Alex <alexthomas1912@gmail.com>
2025-10-22 23:47:46 +0100

    Remove redundant test view url

--- categories/urls.py
+++ categories/urls.py
@@ -5,5 +5,4 @@ urlpatterns = [
     path('<slug:slug>/', views.category_detail, name='category_detail'),
     path('<slug:slug>/edit_log/<int:log_id>/', views.log_edit, name='log_edit'),
     path('<slug:slug>/delete_log/<int:log_id>/', views.log_delete, name='log_delete'),
-    path('test/', views.test_view, name='categories-test'),
 ]
