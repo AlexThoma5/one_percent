dec616bb99cde714ab2bf56a3e733d03607ded20
Alex <alexthomas1912@gmail.com>
2025-10-28 17:42:32 +0000

    Remove unnecessary whitenoise setting in settings.py

--- one_percent/settings.py
+++ one_percent/settings.py
@@ -154,9 +154,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-# Force cache-busting for static files
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage" # noqa
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
